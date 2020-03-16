79dd4f552bb5b64c71598838fc7dd56bb7bd5869
Matheus Moraes <matheusmoraes@MacBook-de-Matheus.local>
2020-03-16 08:26:47 -0300

    a

--- js/carrossel.js
+++ js/carrossel.js
@@ -8,8 +8,8 @@ $("#carouselpropostas").owlCarousel({
     autoPlay: 3000, //Set AutoPlay to 3 seconds
 
     items : 2,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [979,4],
+    itemsDesktop : [1199,2],
+    itemsDesktopSmall : [979,2],
     navigation: false,
     stopOnHover: true,
     pagination: false
