c0e4deee8df27cda0d2208a08923926cd1ada5e7
BeatrizRoisin13 <beatriz.roisin@cbykonne.com>
2021-09-13 17:01:13 -0300

    ajuste

--- js/cart_helperCBYK.js
+++ js/cart_helperCBYK.js
@@ -1,38 +1,37 @@
 
 
-   function openNav() {
+function openNav() {
 
-    const mq = window.matchMedia("(max-width: 600px)");
-    $("body").addClass("menu-ativo");
-    if (mq.matches) {
-     // document.getElementById("mySidenav").style.width = `${document.documentElement.clientWidth}px`;
-     document.getElementById("mySidenav").style.right = "0px";
-    } else{
-      //document.getElementById("mySidenav").style.width = "485px";
-      document.getElementById("mySidenav").style.right = "0px";
-    }
-    }
-    
+  const mq = window.matchMedia("(max-width: 600px)");
+  $("body").addClass("menu-ativo");
+  if (mq.matches) {
+    // document.getElementById("mySidenav").style.width = `${document.documentElement.clientWidth}px`;
+    document.getElementById("mySidenav").style.right = "0px";
+  } else {
+    //document.getElementById("mySidenav").style.width = "485px";
+    document.getElementById("mySidenav").style.right = "0px";
+  }
+}
+
 
 function closeNav() {
   $("body").removeClass("menu-ativo");
-    document.getElementById("mySidenav").style.right = "-880px";
+  document.getElementById("mySidenav").style.right = "-880px";
 }
 
 
 
-function formatReal( int )
-{
-        var tmp = int+'';
-        tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
-        if( tmp.length > 6 )
-                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+function formatReal(int) {
+  var tmp = int + '';
+  tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
+  if (tmp.length > 6)
+    tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
 
-        return tmp;
+  return tmp;
 }
 
-setTimeout(()=>{
-    $("#mini-cart-admake .mini-cart-item .detalhes").append(`
+setTimeout(() => {
+  $("#mini-cart-admake .mini-cart-item .detalhes").append(`
     <button class="btn btn-menos-check" style="background-color: transparent;outline-style: none;
         box-shadow: none;"><i class="fa fa-minus"></i></button>
             <input type = "number" class="qtd-field" value = "1" style = "width: 50px;
@@ -74,14 +73,14 @@ fill: #025EB9;
         
     `);
 
-    $(".qtd-field").prop("disabled", true);
-    $("#mini-cart-admake .mini-cart-itens").html("")
- vtexjs.checkout.getOrderForm()
-.then(function(orderForm) {
+  $(".qtd-field").prop("disabled", true);
+  $("#mini-cart-admake .mini-cart-itens").html("")
+  vtexjs.checkout.getOrderForm()
+    .then(function (orderForm) {
 
 
-  orderForm.items.forEach((element, index) => {
-    $("#mini-cart-admake .mini-cart-itens").append(`
+      orderForm.items.forEach((element, index) => {
+        $("#mini-cart-admake .mini-cart-itens").append(`
 							
 								
     <div class="mini-cart-item item-${index}">
@@ -138,115 +137,139 @@ fill: #025EB9;
   
     </div>
   `);
-  });
+
+
+      });
+
+
+
+   
+
+      $("#mini-cart-admake-total").text("R$ " + formatReal(orderForm.value));
 
 
 
 
-$("#mini-cart-admake-total").text("R$ " + formatReal(orderForm.value));
 
 
 
 
 
-
-
-
-
-let miniNumber = 0
-    for(let i = 0; i < orderForm.items.length; i++) {
+      let miniNumber = 0
+      for (let i = 0; i < orderForm.items.length; i++) {
         $($(".qtd-field")[i]).val(orderForm.items[i].quantity)
         miniNumber += orderForm.items[i].quantity
-    }
-    $(".mini-cart-qty-admake").text(`${miniNumber}`)
+      }
+      $(".mini-cart-qty-admake").text(`${miniNumber}`)
 
-    $("#mini-cart-admake-total").text("R$ " + formatReal(orderForm.value));
- //formatReal(orderForm.value)
-});
-
-
-},2500);
+      $("#mini-cart-admake-total").text("R$ " + formatReal(orderForm.value));
+      //formatReal(orderForm.value)
+    });
 
 
+}, 2500);
 
 
-$(document).on('click', ".btn-mais-check",  function(event) {
+
+
+$(document).on('click', ".btn-mais-check", function (event) {
   var rowindex = $(this).closest('.mini-cart-item').index();
   console.log('rowindex', rowindex);
-  let me =  $(".qtd-field")[rowindex];
- let value = parseInt($($(".qtd-field")[rowindex]).val())
- console.log(me, value);
+  let me = $(".qtd-field")[rowindex];
+  let value = parseInt($($(".qtd-field")[rowindex]).val())
+  console.log(me, value);
 
 
-     value +=  1;
-     console.log(value)
-     $($("#mini-cart-admake .mini-cart-item .qtd-valor .qtd")[rowindex]).text( `${value} X`)
- // $(me).val(value)
+  value += 1;
+  console.log(value)
+  $($("#mini-cart-admake .mini-cart-item .qtd-valor .qtd")[rowindex]).text(`${value} X`)
+  // $(me).val(value)
   $($(".qtd-field")[rowindex]).val(value)
 
   vtexjs.checkout.getOrderForm()
-.then(function(orderForm) {
-  var itemIndex = 0;
-  var item = orderForm.items[rowindex];
-  var updateItem = {
-    index: rowindex,
-    quantity:  value
-  };
-  return vtexjs.checkout.updateItems([updateItem], null, false);
-})
-.done(function(orderForm) {
-  //alert('Items atualizados!');
-  console.log(orderForm);
-  $("#mini-cart-admake-total").text("R$ " + formatReal(orderForm.value));
-});
+    .then(function (orderForm) {
+      var itemIndex = 0;
+      var item = orderForm.items[rowindex];
+      var updateItem = {
+        index: rowindex,
+        quantity: value
+      };
+      return vtexjs.checkout.updateItems([updateItem], null, false);
+    })
+    .done(function (orderForm) {
+      //alert('Items atualizados!');
+      console.log(orderForm);
+      $("#mini-cart-admake-total").text("R$ " + formatReal(orderForm.value));
+    });
 
 });
 
-$(document).on('click',".btn-menos-check",  function(event) {
+$(document).on('click', ".btn-menos-check", function (event) {
   var rowindex = $(this).closest('.mini-cart-item').index();
   console.log('rowindex', rowindex);
-  let me =  $(".qtd-field")[rowindex];
- let value = parseInt($($(".qtd-field")[rowindex]).val())
- console.log(me, value);
+  let me = $(".qtd-field")[rowindex];
+  let value = parseInt($($(".qtd-field")[rowindex]).val())
+  console.log(me, value);
 
- if (value > 1) {
-     console.log("entrei")
-     value -=  1;
-     console.log(value)
-     $($("#mini-cart-admake .mini-cart-item .qtd-valor .qtd")[rowindex]).text(`${value} X`)
-  
- // $(me).val(value)
-  $($(".qtd-field")[rowindex]).val(value)
+  if (value > 1) {
+    console.log("entrei")
+    value -= 1;
+    console.log(value)
+    $($("#mini-cart-admake .mini-cart-item .qtd-valor .qtd")[rowindex]).text(`${value} X`)
 
-  vtexjs.checkout.getOrderForm()
-.then(function(orderForm) {
-  var itemIndex = 0;
-  var item = orderForm.items[rowindex];
-  var updateItem = {
-    index: rowindex,
-    quantity:  value
-  };
-  return vtexjs.checkout.updateItems([updateItem], null, false);
-})
-.done(function(orderForm) {
-  //alert('Items atualizados!');
-  console.log(orderForm);
-  $("#mini-cart-admake-total").text("R$ " + formatReal(orderForm.value));
+    // $(me).val(value)
+    $($(".qtd-field")[rowindex]).val(value)
+
+    vtexjs.checkout.getOrderForm()
+      .then(function (orderForm) {
+        var itemIndex = 0;
+        var item = orderForm.items[rowindex];
+        var updateItem = {
+          index: rowindex,
+          quantity: value
+        };
+        return vtexjs.checkout.updateItems([updateItem], null, false);
+      })
+      .done(function (orderForm) {
+        //alert('Items atualizados!');
+        console.log(orderForm);
+        $("#mini-cart-admake-total").text("R$ " + formatReal(orderForm.value));
+      });
+  }
 });
-}
-});
 
 
-$(document).on('click', ".product-remover", function(event){
+$(document).on('click', ".product-remover", function (event) {
   var rowindex = $(this).closest('.mini-cart-item').index();
-  let itemsToRemove = [{"index": rowindex,
-  "quantity": 0}]
-  vtexjs.checkout.removeItems(itemsToRemove).then(res=>{
+  let itemsToRemove = [{
+    "index": rowindex,
+    "quantity": 0
+  }]
+  vtexjs.checkout.removeItems(itemsToRemove).then(res => {
     console.log("removido");
-    
+
     $($(".mini-cart-item")[rowindex]).remove();
     $("#mini-cart-admake-total").text("R$ " + formatReal(res.value));
 
   });
 })
 
+// //JQuery(document).ready(function () {
+
+//   //esta é uma alternativa mas pode falhar quando existem espaços em branco dentro da tag
+//   var test = jQuery("#mini-cart-itens").html();
+//   if ($('.mini-cart-item').contents().length == 0) {
+//     $(".continue-comprando").css('display', 'flex');
+//     $(".mini-cart-botao").css('display', 'none');
+//     $(".mini-cart-totais").css('display', 'none');
+//      //elemento vazio.. faça algo
+//       alert("vazio");
+//   } else {
+//     $(".continue-comprando").css('display', 'none');
+//     $(".mini-cart-botao").css('display', 'block');
+//     $(".mini-cart-totais").css('display', 'block');
+//      //element possui html dentro...
+//       alert("não vazio");
+//   }
+
+// });
--- js/newsletterCBYK.js
+++ js/newsletterCBYK.js
@@ -1,44 +1,65 @@
 
-$("#commit").click(function (event) {
-    event.preventDefault();
+// $("#emailcatcher").click(function (event) {
+//     event.preventDefault();
   
 
-        let myname = $("#nameform").val();
-        let myemail = $("#emailform").val();
-        console.log(myname, myemail)
-        __blc['id'] = "7f4e5c0c8a2e86f3ede7dc38246bddf1";
+//         let myname = $("#nameform").val();
+//         let myemail = $("#emailform").val();
+//         console.log(myname, myemail)
+//         __blc['id'] = "7f4e5c0c8a2e86f3ede7dc38246bddf1";
 
-        try {
+//         try {
 
-            lc.sendData({
-                evento: "Novo Cadastro Newsletter",
-                nm_email: myemail,
-                vars: {
-                    nome: myname,
-                },
-                vars_json: {
+//             lc.sendData({
+//                 evento: "Novo Cadastro Newsletter",
+//                 nm_email: myemail,
+//                 vars: {
+//                     nome: myname,
+//                 },
+//                 vars_json: {
 
-                },
-                lista: {
-                    nm_lista: "newsletter_adc",
-                    atualizar: "1",
-                    nome: myname
+//                 },
+//                 lista: {
+//                     nm_lista: "newsletter_adc",
+//                     atualizar: "1",
+//                     nome: myname
 
-                }
-            });
+//                 }
+//             });
 
-          alert("Inscrição concluída!")
+//           alert("Inscrição concluída!")
 
-        } catch (e) {
-            alert("Algo de errado aconteceu :( \n Tente mais tarde")
-        }
+//         } catch (e) {
+//             alert("Algo de errado aconteceu :( \n Tente mais tarde")
+//         }
     
 
 
 
-})
+// })
 
-
+$("#emailcatcher").on('click', ()=>{
+    event.preventDefault(); 
+    //console.log($("#cl_email").val())
+    let dados = {
+        Email : $("#emailform").val(),
+        Nome : $("#nameform").val('nome').hide()
+    }
+  
+    fetch("/api/dataentities/NL/documents", {
+        method: 'PATCH',
+               headers: {
+                "Content-Type": "application/json"
+                  },
+        body: JSON.stringify(dados)
+    })
+    .then((res) => {return res})
+    .then(result =>{
+        console.log(result);
+        alert("Inscrição concluída");
+    })
+    .catch(err => console.log(err))
+  })
 
 
 
