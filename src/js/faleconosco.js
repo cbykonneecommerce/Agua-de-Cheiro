af9b7d413e0fde1d4fe689c40c35067008e0874c
DeboraMoraes <86421596+DebMoraes19@users.noreply.github.com>
2022-01-26 13:07:20 -0300

    Revert "Merge branch 'master' of https://github.com/cbykonneecommerce/Agua-de-Cheiro"
    This reverts commit 02a86e09c9d562ca922622fe48ff7172178e3cbf, reversing
    changes made to 8a8bf31a597e93fe878d0ac16d13be6fe9e86cda.

--- src/js/faleconosco.js
+++ src/js/faleconosco.js
@@ -1,100 +1,261 @@
+// $("#cl_home_phone").on("keyup", function(e){
+
+//     let tel = $(this).val();
+//     tel=tel.replace(/\D/g,"")
+//     tel=tel.replace(/^(\d)/,"($1")
+//     tel=tel.replace(/(.{3})(\d)/,"$1)$2")
+//     if(tel.length == 9) {
+//         tel=tel.replace(/(.{1})$/,"-$1")
+//     } else if (tel.length == 10) {
+//         tel=tel.replace(/(.{2})$/,"-$1")
+//     } else if (tel.length == 11) {
+//         tel=tel.replace(/(.{3})$/,"-$1")
+//     } else if (tel.length == 12) {
+//         tel=tel.replace(/(.{4})$/,"-$1")
+//     } else if (tel.length > 12) {
+//         tel=tel.replace(/(.{4})$/,"-$1")
+//     }
+//  $(this).val(tel);
+//  });
+
+
+//  $("#cl_cpf").on("keyup", function(e){
+//     $(this).val(
+//         $(this).val()
+//         .replace(/\D/g,"")
+//         .replace(/(\d{3})(\d)/,"$1.$2")
+//         .replace(/(\d{3})(\d)/,"$1.$2")
+//         .replace(/(\d{3})(\d{1,2})$/,"$1-$2"));
+// });
+
+
+
+
+
+
+
+// function ContactCreate(storeName, dataEntity, co_client)
+// {
+// 	var co_description 	= $("#co_description").val();
+// 	var co_type 		= $("#co_type").val();
+
+// 	var jsonCO = 	{
+// 					"client": co_client.replace("CL-",""),
+// 					"description": co_description,
+// 					"type": co_type
+// 					};
+
+// 	var urlCO = "/api/dataentities/" + dataEntity + "/documents/";
+
+// 	$.ajax({
+// 		headers: {
+// 			"Accept": "application/vnd.vtex.ds.v10+json",
+// 			"Content-Type": "application/json"
+// 		},
+// 		data: JSON.stringify(jsonCO),
+// 		type: 'PATCH',
+// 		url: urlCO,
+// 		success: function (data) {
+// 		  console.log(data);
+// 		  ResetMessages()
+// 		  $("#co_message_success").show();
+// 		  $("#cl_first_name").val("");
+// 		  $("#cl_cpf").val("");
+// 		  $("#cl_email").val("");
+// 		  $("#cl_home_phone").val("");
+// 		  $("#co_description").val("");
+// 		},
+// 		error: function (data) {
+// 		  console.log(data);
+// 		  ResetMessages()
+// 		  $("#co_message_error").show();
+// 		}
+// 	});
+// }
+
+// function ContactCreateByEmail(storeName, dataEntity, cl_email)
+// {
+// 	/*var cl_url = "/api/dataentities/CL/search/?email=" + cl_email + "&_fields=id";
+
+// 	$.ajax({
+// 		headers: {
+// 			"Accept": "application/vnd.vtex.ds.v10+json",
+// 			"Content-Type": "application/json"
+// 		},
+// 		type: 'GET',
+// 		url: cl_url,
+// 		success: function(data, textStatus, xhr){
+// 			console.log(data);
+// 			if(xhr.status == "200" || xhr.status == "201"){
+// 				ContactCreate(storeName, dataEntity, data[0].id);
+// 			}else{
+// 				ResetMessages()
+// 				$("#co_message_error").show();
+// 			}
+// 		},
+// 		error: function(data){
+// 			console.log(data);
+// 			ResetMessages()
+// 			$("#co_message_error").show();
+// 		}
+// 	});*/
+// }
+
+// function ClientCreate()
+// {
+// 	var storeName		= $("#master_data_store_name").val();
+// 	var dataEntity		= $("#master_data_data_entity").val();
+
+// 	var cl_first_name 	= $("#cl_first_name").val();
+// 	var cl_email 		= $("#cl_email").val();
+// 	var cl_home_phone 	= $("#cl_home_phone").val();
+//     var cl_description  = $("#co_description").val();
+//     var cl_cpf          = $("#cl_cpf").val();
+
+// 	var cl_json = 	{
+// 					"firstName": cl_first_name,
+// 					"email": cl_email,
+// 					"homePhone": cl_home_phone,
+//                     "description": cl_description,
+//                     "cpf": cl_cpf
+// 					};
+
+// 	var cl_url = "/api/dataentities/CO/documents/";
+
+// 	$.ajax({
+// 		headers: {
+// 			"Accept": "application/vnd.vtex.ds.v10+json",
+// 			"Content-Type": "application/json"
+// 		},
+// 		data: JSON.stringify(cl_json),
+// 		type: 'PATCH',
+// 		url: cl_url,
+// 		success: function(data, textStatus, xhr){
+// 			console.log(data);
+// 			if(xhr.status == "200" || xhr.status == "201"){
+// 				ResetMessages()
+// 				$("#co_message_success").show();
+// 			}else if(xhr.status == "304"){
+// 				ContactCreateByEmail(storeName, dataEntity, cl_email);
+// 			}else{
+// 				ResetMessages()
+// 				$("#co_message_error").show();
+// 			}
+// 		},
+// 		error: function(data){
+// 			console.log(data);
+// 			ResetMessages()
+// 			$("#co_message_error").show();
+// 		}
+// 	});
+// }
+
+// function ResetMessages()
+// {
+// 	$("#co_message_loading").hide();
+// 	$("#co_message_validate").hide();
+// 	$("#co_message_success").hide();
+// 	$("#co_message_error").hide();
+// }
+
+// function IsEmail(email) {
+//   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+//   return regex.test(email);
+// }
+
+// function FormValidate()
+// {
+// 	var isFormValidate = true;
+
+// 	if($("#cl_first_name").val() == ""){
+// 		isFormValidate = false;
+// 		$("#cl_first_name").focus();
+// 	}
+// 	if((isFormValidate) && ($("#cl_email").val() == "")){
+// 		isFormValidate = false;
+// 		$("#cl_email").focus();
+// 	}
+// 	if((isFormValidate) && (!IsEmail($("#cl_email").val()))){
+// 		isFormValidate = false;
+// 		$("#cl_email").val("");
+// 		$("#cl_email").focus();
+// 	}
+// 	if((isFormValidate) && ($("#cl_home_phone").val() == "")){
+// 		isFormValidate = false;
+// 		$("#cl_home_phone").focus();
+// 	}
+
+// 	if((isFormValidate) && ($("#co_description").val() == "")){
+// 		isFormValidate = false;
+// 		$("#co_description").focus();
+// 	}
+
+// 	if(isFormValidate){
+// 		ResetMessages()
+// 		$("#co_message_loading").show();
+// 		ClientCreate();
+// 	}else{
+// 		ResetMessages()
+// 		$("#co_message_validate").show();
+// 	}
+
+// 	return false;
+// }
+
+
 $("#commit").on('click', () => {
 
-  event.preventDefault();
-  //console.log($("#cl_email").val())
-  let dados = {
-    firstName: $("#cl_first_name").val(),
-    email: $("#cl_email").val(),
-    homePhone: $("#ctTel").val(),
-    cpf: $("#cl_cpf").val(),
-    description: $("#co_description").val()
-  }
+    event.preventDefault();
+    //console.log($("#cl_email").val())
+    let dados = {
+        firstName: $("#cl_first_name").val(),
+        email: $("#cl_email").val(),
+        homePhone: $("#ctTel").val(),
+        cpf: $("#cl_cpf").val(),
+        description: $("#co_description").val()
+    }
 
-  fetch("/api/dataentities/CO/documents", {
-    method: 'PATCH',
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(dados)
-  })
-    .then((res) => { return res })
-    .then(result => {
-      console.log(result);
-      alert("Formulario enviado");
-    })
-    .catch(err => console.log(err))
+    fetch("/api/dataentities/CO/documents", {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dados)
+        })
+        .then((res) => { return res })
+        .then(result => {
+            console.log(result);
+            alert("Formulario enviado");
+        })
+        .catch(err => console.log(err))
 })
 
-$(document).ready(() => {
-  if (~window.location.search.indexOf('test=true')) {
-    $('#institucional-content .row > .col-md-10').html(`
-        <div class="stores">
-          <h4> CENTRAL DE ATENDIMENTO </h4>
-          <h2> Nossas Lojas</h2>
-          <div class="stores__body">
 
-          </div>
-        </div>
-    `)
-
-    getStores();
-
-  }
-})
-
-const getStores = () => {
-  console.log('passei')
-  fetch("/api/dataentities/LS/search?_fields=state,city,neighborhood,address,contact", {
+fetch("/api/dataentities/LS/search?_fields=state,city,neighborhood,address,contact", {
     method: 'GET',
     headers: {
-      "REST-Range": "resources=0-999",
-      "Accept": "application/vnd.vtex.masterdata.v10+json",
-      "Content-Type": "application/vnd.vtex.masterdata.v10+json"
-    }
-  })
-    .then((res) => { return res.json() })
-    .then((result) => {
-      const state = result.map((item) => {
-        return item.state
-      }).filter((item, index, array) => {
-        return array.indexOf(item) === index
-      })
+        "REST-Range": "resources=0-999",
+        "Accept": "application/vnd.vtex.masterdata.v10+json",
+        "Content-Type": "application/vnd.vtex.masterdata.v10+json"
+    },
+    // body: JSON.stringify(dados)
+})
 
-      renderStates(state)
-      renderCities(result)
+.then((res) => { return res.json() })
+
+.then((result) => {
+        var arr = result.map((item) => {
+            return item.state
+        }).filter((item, index, array) => {
+            return array.indexOf(item) === index
+        })
+        renderStates(arr)
     })
     .catch(err => console.log(err))
-}
 
-const renderStates = (state) => {
-  const stateList = state.map(item => {
-    return (`
-      <div class="stores__state" data-id="${item}">
-        <h4>${item}</h4>
-        <div class="stores__cities">
-          <div class="stores__table stores__table--head">
-            <div class="stores__cell">Cidade</div>
-            <div class="stores__cell">Bairro</div>
-            <div class="stores__cell">Endereço</div>
-            <div class="stores__cell">Contatos</div>
-          </div>
-        </div>
-      </div>
-    `)
-  })
-
-  $('.stores__body').append(stateList)
-}
-const renderCities = (state) => {
-  state.forEach(store => {
-    $('.stores__body').find(`.stores__state[data-id='${store.state}'] .stores__cities`).append(`
-      <div class="stores__table">
-        <div class="stores__cell stores__cell--city">${store.city}</div>
-        <div class="stores__cell">${store.neighborhood}</div>
-        <div class="stores__cell stores__cell--address">${store.address}</div>
-        <div class="stores__cell">${store.contact}</div>
-      </div>
-    `)
-  })
-
+function renderStates(arr) {
+    var arrLength = arr.length;
+    for (var i = 0; i < arrLength; i++) {
+        $(".estados").append(`<h4>${arr[i]}</h4>`)
+    }
 }
