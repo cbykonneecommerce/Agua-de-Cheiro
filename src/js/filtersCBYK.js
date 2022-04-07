01e1ae473ab626e70eeaac0009ee2b30adbd508f
Alison Pinheiro <alisonpinheiro17@gmail.com>
2022-04-06 22:23:40 -0300

    ajustes na prateleira

--- src/js/filtersCBYK.js
+++ src/js/filtersCBYK.js
@@ -1,63 +1,63 @@
 
-setTimeout(function(){
+setTimeout(function () {
 
-    $(".bt-refinar.search-filter-button.even").text("Aplicar Filtros");
-    
-        $(".refino h5").append(`<svg style="width: 26px;
+  $(".bt-refinar.search-filter-button.even").text("Aplicar Filtros");
+
+  $(".refino h5").append(`<svg style="width: 26px;
         max-width: 26px;
         float: right;
         margin-top: -9px;" xmlns="http://www.w3.org/2000/svg" width="50.523" height="39.523" viewBox="0 0 50.523 39.523">
         <path fill="none" stroke="rgb(0,0,0)" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="5" d="M11.76158875 11.76155253L25.7615618 27.76157612l13.00002696-15.9999781"/>
       </svg>
       `);
-    
-    
-    $("fieldset.refino h5").on("click",function(){
-        $(this).parent().toggleClass( "active")
-    })
-      
-    
-    
-    
-      $("fieldset.refino div label").prepend(`<i class="fa fa-square-o"></i>`);
-    
-      $("fieldset.refino div label").on("click", function(){
-    if($("input", this).is(":checked")){
-        $("i", this).attr('class',"fa fa-check-square-o")
+
+
+  $("fieldset.refino h5").on("click", function () {
+    $(this).parent().toggleClass("active")
+  })
+
+
+
+
+  $("fieldset.refino div label").prepend(`<i class="fa fa-square-o"></i>`);
+
+  $("fieldset.refino div label").on("click", function () {
+    if ($("input", this).is(":checked")) {
+      $("i", this).attr('class', "fa fa-check-square-o")
     } else {
-        $("i", this).attr('class',"fa fa-square-o")
+      $("i", this).attr('class', "fa fa-square-o")
     }
-     
-     })
+
+  })
 
 
 
 
-     $(".refino-marca h5").append(`<svg style="width: 26px;
+  $(".refino-marca h5").append(`<svg style="width: 26px;
      max-width: 26px;
      float: right;
      margin-top: -9px;" xmlns="http://www.w3.org/2000/svg" width="50.523" height="39.523" viewBox="0 0 50.523 39.523">
      <path fill="none" stroke="rgb(0,0,0)" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="5" d="M11.76158875 11.76155253L25.7615618 27.76157612l13.00002696-15.9999781"/>
    </svg>
    `);
- 
- 
- $("fieldset.refino-marca h5").on("click",function(){
-     $(this).parent().toggleClass( "active")
- })
-   
- 
- 
- 
-   $("fieldset.refino-marca div label").prepend(`<i class="fa fa-square-o"></i>`);
- 
-   $("fieldset.refino-marca div label").on("click", function(){
- if($("input", this).is(":checked")){
-     $("i", this).attr('class',"fa fa-check-square-o")
- } else {
-     $("i", this).attr('class',"fa fa-square-o")
- }
-  
+
+
+  $("fieldset.refino-marca h5").on("click", function () {
+    $(this).parent().toggleClass("active")
+  })
+
+
+
+
+  $("fieldset.refino-marca div label").prepend(`<i class="fa fa-square-o"></i>`);
+
+  $("fieldset.refino-marca div label").on("click", function () {
+    if ($("input", this).is(":checked")) {
+      $("i", this).attr('class', "fa fa-check-square-o")
+    } else {
+      $("i", this).attr('class', "fa fa-square-o")
+    }
+
   })
 
 
@@ -65,26 +65,27 @@ setTimeout(function(){
 
 
 
-    
-     $( "a.ver-filtros" ).siblings("label").children("i").attr('class',"fa fa-check-square-o")
 
-     $(".resultado-busca-filtro > fieldset.orderBy select option:first-of-type").text("Ordenar por");
-    
-    }, 1000)
-    
-    setInterval(() => {
-        $(".bt-refinar.search-filter-button.even").text("Aplicar Filtros");
-    }, 1000);
+  $("a.ver-filtros").siblings("label").children("i").attr('class', "fa fa-check-square-o")
 
+  $(".resultado-busca-filtro > fieldset.orderBy select option:first-of-type").text("Ordenar por");
 
+}, 1000)
 
+setInterval(() => {
+  $(".bt-refinar.search-filter-button.even").text("Aplicar Filtros");
+  $('.prateleira ul li.helperComplement').remove()
+}, 1000);
 
 
-    const mq = window.matchMedia("(max-width: 800px)");
 
-    if (mq.matches) {
-      $(".resultado-busca-filtro").append(`
-    
+
+
+const mq = window.matchMedia("(max-width: 800px)");
+
+if (mq.matches) {
+  $(".resultado-busca-filtro").append(`
+
       <span onclick="openNavfiltro()" style="
       border: 1px solid #F2F2F2 !important;
       border-radius: 0 !important;
@@ -97,6 +98,6 @@ setTimeout(function(){
       text-align: center;
   ">Filtros <img src="/arquivos/filtro.svg" style="max-width: 25px;margin-left: 10px;"></span>
       `)
-    }
- 
-   
+}
+
+
