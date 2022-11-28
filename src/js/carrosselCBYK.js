55ce6ff0e950324291f32ef0b2d0383c424dbd4a
Eris <34255207+Erislandio@users.noreply.github.com>
2022-11-28 11:23:26 -0300

    ajustes

--- src/js/carrosselCBYK.js
+++ src/js/carrosselCBYK.js
@@ -307,7 +307,7 @@ const innerHTMLContagem = `<div class="cb-landing-countdown" id="cybermonday-rel
 </div>`
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 26, 2022 00:00:00").getTime();
+var countDownDate = new Date("Nov 28, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
