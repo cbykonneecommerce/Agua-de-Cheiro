8d75690f71e0f4a00acf236e8a2c58c94506fda7
Eris <34255207+Erislandio@users.noreply.github.com>
2022-12-14 20:53:30 -0300

    ajustes minicart

--- src/js/headerMakerCBYK.js
+++ src/js/headerMakerCBYK.js
@@ -1,3 +1,68 @@
+function getAllUrlParams(url) {
+
+  // get query string from url (optional) or window
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+  // we'll store the parameters here
+  var obj = {};
+
+  // if query string exists
+  if (queryString) {
+
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+
+    for (var i = 0; i < arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+
+      // set parameter name and value (use 'true' if empty)
+      var paramName = a[0];
+      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+      // (optional) keep case consistent
+      paramName = paramName.toLowerCase();
+      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+      if (paramName.match(/\[(\d+)?\]$/)) {
+
+        // create key if it doesn't exist
+        var key = paramName.replace(/\[(\d+)?\]/, '');
+        if (!obj[key]) obj[key] = [];
+
+        // if it's an indexed array e.g. colors[2]
+        if (paramName.match(/\[\d+\]$/)) {
+          // get the index value and add the entry at the appropriate position
+          var index = /\[(\d+)\]/.exec(paramName)[1];
+          obj[key][index] = paramValue;
+        } else {
+          // otherwise add the value to the end of the array
+          obj[key].push(paramValue);
+        }
+      } else {
+        // we're dealing with a string
+        if (!obj[paramName]) {
+          // if it doesn't exist, create property
+          obj[paramName] = paramValue;
+        } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+          // if property does exist and it's a string, convert it to an array
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+        } else {
+          // otherwise add the property
+          obj[paramName].push(paramValue);
+        }
+      }
+    }
+  }
+
+  return obj;
+}
+
 function userModal() {
   $('body').on('click', '.header__user .dropdown-toggle', function (event) {
     event.preventDefault();
@@ -37,8 +102,8 @@ function verifyUserIsLogged() {
     })
 }
 
-function verifySpecialCampaign () {
-  if($('.js-item-menu a').length){
+function verifySpecialCampaign() {
+  if ($('.js-item-menu a').length) {
     $('body').addClass('campanha-especial')
   }
 }
@@ -332,11 +397,8 @@ $(window).on('orderFormUpdated.vtex', function (evt, orderForm) {
 
 $(document).ready(function () {
   setTimeout(() => {
-    // remove do footer do minicart teste b
-    $('.cart__buttons button.cart__button').remove()
-    $('.sidenavcart').prepend(`<button onclick="$('.cart__close').click()" class='cart__button'>CONTINUAR COMPRANDO</button>`)
-
     const shelfItems = document.querySelectorAll('.box-item .buy-btns a')
+    const productCards = document.querySelectorAll('.wrapper-buy-button-asynchronous a')
 
     for (let index = 0; index < shelfItems.length; index++) {
       const element = shelfItems.item(index);
@@ -364,5 +426,33 @@ $(document).ready(function () {
         })
       }
     }
+
+    for (let index = 0; index < productCards.length; index++) {
+      const element = productCards.item(index);
+      const productlink = $(element).prop('href')
+
+      if (productlink && productlink.includes('?')) {
+        const params = getAllUrlParams(productlink)
+
+        $(element).removeAttr("href")
+        $(element).on('click', function () {
+          const item = [{
+            id: params.sku,
+            quantity: 1,
+            seller: params.seller
+          }]
+
+          vtexjs.checkout.getOrderForm().done(() => {
+            vtexjs.checkout.addToCart(item, null, 1).done(() => {
+              // abrir minicart aqui
+              setTimeout(() => {
+                $('.btn-mini-cart').click()
+              }, 500)
+            })
+          })
+        })
+      }
+    }
+
   }, 1000);
 })
